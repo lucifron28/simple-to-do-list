1fce9643fe50651ca4b346c327cfabbcb260cc2e
Ron <lucifron28@gmail.com>
2025-05-19 20:33:37 +0800

    added add task button and dialog

--- lib/app.dart
+++ lib/app.dart
@@ -8,12 +8,7 @@ class ToDoListApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To-do List',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('To-do List'),
-        ),
-        body: ToDoList(),
-      ),
+      home: ToDoList(),
     );
   }
 }
@@ -26,15 +21,59 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
+  Future<void> addTaskDialog(BuildContext context) async {
+    final TextEditingController addTaskController = TextEditingController();
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Task'),
+          content: TextField(
+            controller: addTaskController,
+            decoration: InputDecoration(hintText: 'Add Task Here'),
+          ),
+          actions: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+              child: Container(
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: TextButton(
+                  onPressed: () => Navigator.pop(context), 
+                  style: TextButton.styleFrom(foregroundColor: Colors.white),
+                  child: Text('Close')
+                    ),
+                  ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text("Hello, World"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-do List'),
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                onPressed: () => addTaskDialog(context),
+                icon: Icon(Icons.add),
+              );
+            },
+          ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: Container(color: Colors.grey, height: 1.0),
+        ),
       ),
+      body: Text("Hello World"),
     );
   }
 }

3964bfb33a899da976773bee111a2955ccc00c8e
Ron <lucifron28@gmail.com>
2025-05-19 23:16:49 +0800

    added onDelete callback

--- lib/task_item.dart
+++ lib/task_item.dart
@@ -4,12 +4,14 @@ class TaskItem extends StatelessWidget {
   final String taskName;
   final bool isDone;
   final VoidCallback onToggleDone;
+  final VoidCallback onDelete;
   const TaskItem({
-    super.key, 
+    super.key,
     required this.taskName,
     required this.isDone,
     required this.onToggleDone,
-    });
+    required this.onDelete
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -19,22 +21,27 @@ class TaskItem extends StatelessWidget {
         decoration: BoxDecoration(
           border: Border.all(color: Colors.black),
           borderRadius: BorderRadius.all(Radius.circular(12.0)),
-          ),
+        ),
         child: ListTile(
           title: Center(
             child: Text(
               taskName,
-              style: TextStyle(decoration: isDone ? TextDecoration.lineThrough : null),
+              style: TextStyle(
+                decoration: isDone ? TextDecoration.lineThrough : null,
               ),
+            ),
           ),
           leading: IconButton(
             onPressed: onToggleDone,
             icon: Icon(
               isDone ? Icons.check_box : Icons.check_box_outline_blank,
               color: isDone ? Colors.green : null,
-              ),
             ),
-          trailing: Icon(Icons.delete),
+          ),
+          trailing: IconButton(
+            onPressed: onDelete,
+            icon: Icon(Icons.delete, color: Colors.red,),
+            ),
         ),
       ),
     );

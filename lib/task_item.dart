9cb2b298cbad85b09c4e63dbe2904996308af376
Ron <lucifron28@gmail.com>
2025-05-19 21:24:39 +0800

    add callback

--- lib/task_item.dart
+++ lib/task_item.dart
@@ -3,14 +3,28 @@ import 'package:flutter/material.dart';
 class TaskItem extends StatelessWidget {
   final String taskName;
   final bool isDone;
+  final VoidCallback onToggleDone;
   const TaskItem({
     super.key, 
     required this.taskName,
     required this.isDone,
+    required this.onToggleDone,
     });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile();
+    return ListTile(
+      title: Text(
+        taskName,
+        style: TextStyle(decoration: isDone ? TextDecoration.lineThrough : null),
+        ),
+      trailing: IconButton(
+        onPressed: onToggleDone,
+        icon: Icon(
+          isDone ? Icons.check_box_outline_blank : Icons.check_box,
+          color: isDone ? Colors.green : null,
+          ),
+        ),
+    );
   }
 }
